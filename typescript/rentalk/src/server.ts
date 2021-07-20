797acc1d6c5848b4349885cfca223e7f97843b83
Silveira Guimaraes <silveira_oi@yahoo.com.br>
2021-07-20 23:33:31 +0000

    model category created

--- typescript/rentalk/src/server.ts
+++ typescript/rentalk/src/server.ts
@@ -1,9 +1,8 @@
 import express from "express";
-
+import { categoriesRoutes } from "./routes/categories.routes";
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({ message: "hello world" });
-});
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("Running on port 3333"));
