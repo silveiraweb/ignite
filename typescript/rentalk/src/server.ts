04f648092304a2fb08db7f0fe47debd4887e2dc9
Silveira Guimaraes <silveira_oi@yahoo.com.br>
2021-07-20 21:46:56 +0000

    Inicio

--- typescript/rentalk/src/server.ts
+++ typescript/rentalk/src/server.ts
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  return res.json({ message: "hello world" });
+});
+
 app.listen(3333, () => console.log("Running on port 3333"));
