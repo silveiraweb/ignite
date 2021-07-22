582ad271b58cb398b57f626f474958431304f52b
Silveira Guimaraes <silveira_oi@yahoo.com.br>
2021-07-22 22:19:02 +0000

    cap 3

--- typescript/rentalk/src/routes/categories.routes.ts
+++ typescript/rentalk/src/routes/categories.routes.ts
@@ -1,21 +1,21 @@
 import { Router } from "express";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryServie } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
     const { name, description } = req.body;
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
-    if(categoryAlreadyExists){
-        return res.status(400).json({error: "Category already exists"})
-    }
-    categoriesRepository.create({ name, description})
+    const createCategoryServie = new CreateCategoryServie(categoriesRepository);
+    createCategoryServie.execute({name, description});
 
     return res.status(201).send();
 });
 categoriesRoutes.get("/", (req, res) => {
-    const all = categoriesRepository.list()
-    return res.json(all);
-})
+    const categories = categoriesRepository.list();
+
+    return res.json({ categories });
+});
+
 export { categoriesRoutes };
