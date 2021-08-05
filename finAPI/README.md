7898de3657772a6bb6a4961c00516f355834b991
Silveira Guimaraes <silveira_oi@yahoo.com.br>
2021-08-05 17:10:11 +0000

    final

--- finAPI/README.md
+++ finAPI/README.md
@@ -3,20 +3,21 @@
 ### Requisitos
 
 - [x] Deve ser possivel criar uma conta
-- [] Deve ser possivel buscar o extrato bancario do cliente
-- [] Deve ser possivel realiar um deposito
-- [] Deve ser possivel realizar um saque
-- [] Deve ser possivel buscar o extrato bancario do clientte por data
-- [] Deve ser possivel atualizar os dados do cliente
-- [] Deve ser possivel obter dados da conta do cliente
-- [] Deve ser possivel deletar uma conta
+- [x] Deve ser possivel buscar o extrato bancario do cliente
+- [x] Deve ser possivel realiar um deposito
+- [x] Deve ser possivel realizar um saque
+- [x] Deve ser possivel buscar o extrato bancario do clientte por data
+- [x] Deve ser possivel atualizar os dados do cliente
+- [x] Deve ser possivel obter dados da conta do cliente
+- [x] Deve ser possivel deletar uma conta
+- [] Retornar  o  balance da conta
 
 ## Regras de negócio
 
-- [] Não deve ser possivel cadastrar uma conta com CPF já existente
-- [] Não deve ser possivel fazer deposito em uma conta não existente
-- [] Não deve ser possivel buscar extrato em uma conta não existente
-- [] Não deve ser possivel fazer saque em uma contta não existente
-- [] Não deve ser possivel excluir uma conta nõa existente
-- [] Não deve ser possivel fazer saque quando o saldo  for insuficiente
+- [x] Não deve ser possivel cadastrar uma conta com CPF já existente
+- [x] Não deve ser possivel buscar extrato em uma conta não existente
+- [x] Não deve ser possivel fazer deposito em uma conta não existente
+- [x] Não deve ser possivel fazer saque em uma contta não existente
+- [x] Não deve ser possivel excluir uma conta nõa existente
+- [x] Não deve ser possivel fazer saque quando o saldo  for insuficiente
 
--- finAPI/src/index.js
+++ finAPI/src/index.js
@@ -4,21 +4,51 @@ const app = express();
 
 app.use(express.json());
 
+
 const customers = [];
+/**
+ * id - obj  uuid
+ * cpf - string
+ * name -string
+ * statement []
+ */
+
+// middleware
+function  verifyIfExistsAccountCPF(req, res, next){
+    const { cpf } = req.headers;
+
+	const customer = customers.find((customer) => customer.cpf === cpf);
+	if (!customer) {
+		return res.status(400).json({ error: "Customer not found" });
+	}
+
+    req.customer = customer;
+    return next();
+}
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type ===  'credit'){
+            return acc  + operation.amount;
+        }  else {
+            return acc - operation.amount;
+        }
+    }, 0); // valor inicial do reduce = 0
+    return balance;
+}
+
 
 app.get("/", (req, res) => {
 	return res.send("FinAPI accounts");
 });
 
-/**
- * 
- */
+//  inserindo dados via body
 app.post("/account", (req, res) => {
     const {name, cpf} = req.body;
     const customerAlreadyExists = customers.some(
-        (customer) => customer.cpf);
+        (customer) => customer.cpf === cpf
+        );
     if(customerAlreadyExists){
-        return res.status(400).json("Customer already exists!");
+        return res.status(400).json({error: "Customer already exists!"});
     }
     customers.push({
         id: uuidv4(),
@@ -29,9 +59,88 @@ app.post("/account", (req, res) => {
     });
     return res.status(200).send(customers);
 });
+//  parametros via params  passados na url
+app.get("/statement/:cpf", (req, res) => {
+    const  { cpf } = req.params;
+    
+    const customer = customers.find(
+        (customer) => customer.cpf  === cpf
+    );
+        if(!customer){
+            return res.status(400).json({error: "Customer not found"})
+        }
+    return res.json(customer.statement);
+});
+//  parametros via headers, middleware passado pelo metodo
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
+	const { customer } = req;  // recuperando customer via request
+	return res.json(customer.statement);
+});
+app.get("/statement/date", verifyIfExistsAccountCPF, (req, res) => {
+	const { customer } = req; // recuperando customer via request
+    const { date } = req.query;
+    const dateFormat  = new Date(date + "00:00");
+    const statement = customer.statement.filter((statement) =>  statement.created_at.toDateString() === new Date(dateFormat).toDateString())
 
 
+	return res.json(statement);
+});
+app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
+    const { description, amount } = req.body;
+	const { customer } = req; // recuperando customer via request headers
 
+    const statementOperation =  {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+	return res.json(customer.statement).status(200);
+});
+app.post("/withdraw",  verifyIfExistsAccountCPF, (req, res) => {
+    const { amount } = req.body;
+    const { customer } = req;
+    const balance = getBalance(customer.statement);
+    if(balance < amount){
+        return res.status(400).json({error: "Insufficient founds"})
+    }
+
+    const statementOperation = {
+		amount,
+		created_at: new Date(),
+		type: "debit",
+	};
+    customer.statement.push(statementOperation);
+	return res.json(customer.statement).status(201);
+})
+app.put("/account", verifyIfExistsAccountCPF, (req, res) => {
+    const {  name } = req.body;
+    const { customer } = req;
+    customer.name = name;
+    return res.status(201).send();
+});
+app.get("/account", verifyIfExistsAccountCPF, (req, res)  => {
+    const { customer } = req;
+    return res.json(customer);
+})
+app.delete("/account", verifyIfExistsAccountCPF, (req, res)  => {
+    const { customer } = req;
+    // splice
+    customers.splice(customer, 1);
+
+    return res.json(customers).status(200);
+});
+app.get("/balance", verifyIfExistsAccountCPF, (req,  res) => {
+    const  { customer } = req;
+    const balance = getBalance(customer.statement);
+    return  res.json(balance);
+})
+
+
+// ativando middleware para todos  abaixo
+// app.use(verifyIfExistsAccountCPF);
 
 
 app.listen(3001, () => console.log("Running..."))
